62a9b357f47a7c66d100bf7a25f2ee2ddddb8eee
Adwait Athale <aathale@sonikpass.com>
2021-03-11 18:27:23 -0800

    asfasFasf

--- service/util.py
+++ service/util.py
@@ -4,8 +4,6 @@ import logging
 from typing import List
 
 import boto3 as boto3
-from boto.file import Key
-from boto.s3.connection import S3Connection
 from botocore.exceptions import ClientError
 
 from hubub_common.redis import redisget, redisdel, redisset, RedisDatabase
@@ -15,46 +13,46 @@ from hubub_common.util import (
 )
 
 
-def upload_images_to_s3(app, user_id, images: List[str], encoding: str) -> str:
-    """
-
-    :param app: Current Servoce
-    :param user_id: (int) User.id
-    :param images_path: (str) Path for images. <user_id>/<images_path>/[images]
-    :param images: List of images
-    :param encoding: (str) encoding for images
-    :return: (str) Path to the images
-    """
-
-    logging.getLogger().info("Got {0} images for user:{1}".format(len(images), user_id))
-    if app.hububconfig.get("IGNORE_SERVICE_CREDENTIALS"):
-        return 'client_id', 'client_secret'
-
-    assert user_id is not None
-    aws_access_key = app.hububconfig.get('aws_access_key')
-    assert aws_access_key is not None
-
-    aws_secret_key = app.hububconfig.get('aws_secret_key')
-    assert aws_secret_key is not None
-
-    conn = S3Connection(aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
-    bucket_name = app.hububconfig.get('S3_FACIAL_IMAGES_BUCKET_NAME')
-    assert bucket_name is not None
-    bucket = conn.get_bucket(bucket_name)
-    for image in images:
-        filename = '/'.join([
-            app.hububconfig.get('S3_FACIAL_PREFIX'),
-            '/'.join([str(user_id)]),
-            '.'.join([generate_secret_key(64), encoding])
-        ])
-        k = Key(bucket, filename)
-        image = base64.b64decode(image)
-        k.set_contents_from_string(image)
-    path = '/'.join([
-        app.hububconfig.get('S3_FACIAL_PREFIX'),
-        '/'.join([str(user_id)])])
-
-    return path
+# def upload_images_to_s3(app, user_id, images: List[str], encoding: str) -> str:
+#     """
+#
+#     :param app: Current Servoce
+#     :param user_id: (int) User.id
+#     :param images_path: (str) Path for images. <user_id>/<images_path>/[images]
+#     :param images: List of images
+#     :param encoding: (str) encoding for images
+#     :return: (str) Path to the images
+#     """
+#
+#     logging.getLogger().info("Got {0} images for user:{1}".format(len(images), user_id))
+#     if app.hububconfig.get("IGNORE_SERVICE_CREDENTIALS"):
+#         return 'client_id', 'client_secret'
+#
+#     assert user_id is not None
+#     aws_access_key = app.hububconfig.get('aws_access_key')
+#     assert aws_access_key is not None
+#
+#     aws_secret_key = app.hububconfig.get('aws_secret_key')
+#     assert aws_secret_key is not None
+#
+#     conn = S3Connection(aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+#     bucket_name = app.hububconfig.get('S3_FACIAL_IMAGES_BUCKET_NAME')
+#     assert bucket_name is not None
+#     bucket = conn.get_bucket(bucket_name)
+#     for image in images:
+#         filename = '/'.join([
+#             app.hububconfig.get('S3_FACIAL_PREFIX'),
+#             '/'.join([str(user_id)]),
+#             '.'.join([generate_secret_key(64), encoding])
+#         ])
+#         k = Key(bucket, filename)
+#         image = base64.b64decode(image)
+#         k.set_contents_from_string(image)
+#     path = '/'.join([
+#         app.hububconfig.get('S3_FACIAL_PREFIX'),
+#         '/'.join([str(user_id)])])
+#
+#     return path
 
 
 def upload_single_image_to_s3(app, user_id, image: str, encoding: str) -> (str, str):
